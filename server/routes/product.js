e8678cb6c19781e1a0b48d1eff9d087a3910b18e
2minzy <minjilee0423@gmail.com>
2020-08-26 00:59:35 +0900

    Initial Commit, Completed upload product page

---
+++ server/routes/product.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const { Product } = require('../models/Product');
+//=================================
+//             Product
+//=================================
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+let upload = multer({ storage: storage }).single('file');
+
+router.post('/image', (req, res) => {
+  // Save brought images
+  upload(req, res, (err) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    return res.json({
+      success: true,
+      filePath: res.req.file.path,
+      fileName: res.req.file.filename,
+    });
+  });
+});
+
+router.post('/', (req, res) => {
+  // put the datas we brought in the DB
+  const product = new Product(req.body);
+  product.save((err) => {
+    if (err) return res.status(400).json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
+module.exports = router;
